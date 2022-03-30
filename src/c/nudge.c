7abf306d9ac0db88158940dd12ba0d4c745939fa
Claudio <71095411+itsthered1@users.noreply.github.com>
2022-03-30 14:04:42 -0700

    Created ActionMenu Window

--- src/c/nudge.c
+++ src/c/nudge.c
@@ -1,10 +1,48 @@
 #include <pebble.h>
+
 #define NUM_CONTACTS 3
 
 static Window *s_main_window;
 static MenuLayer *s_menu_layer;
+static ActionMenu *s_action_menu;
+static ActionMenuLevel *s_root_level;
 
-static uint16_t get_num_rows_callback(MenuLayer *menu_layer, void *data) {
+static void action_performed_callback(ActionMenu *action_menu, const ActionMenuItem *action, void *context) {
+  //what
+}
+
+static void init_action_menu() {
+  s_root_level = action_menu_level_create(3);
+
+  action_menu_level_add_action(s_root_level, "Emoji", action_performed_callback, NULL);
+  action_menu_level_add_action(s_root_level, "Canned", action_performed_callback, NULL);
+  action_menu_level_add_action(s_root_level, "Voice", action_performed_callback, NULL);
+}
+
+static void select_click_handler(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
+  ActionMenuConfig config = (ActionMenuConfig) {
+    .root_level = s_root_level,
+    .colors = {
+      .background = PBL_IF_COLOR_ELSE(GColorRed, GColorWhite),
+      .foreground = GColorBlack,
+    },
+    .align = ActionMenuAlignCenter
+  };
+  
+  switch (cell_index->row) {
+    case 0:
+      s_action_menu = action_menu_open(&config);
+      break;
+    case 1:
+      s_action_menu = action_menu_open(&config);
+      break;
+    case 2:
+      s_action_menu = action_menu_open(&config);
+      break;
+  }
+}
+
+static uint16_t get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
   return NUM_CONTACTS;
 }
 
@@ -36,21 +74,6 @@ static int16_t get_cell_height_callback(struct MenuLayer *menu_layer, MenuIndex 
     44);
 }
 
-static void select_callback(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
-  // Use the row to specify which item will receive the select action
-  switch (cell_index->row) {
-    case 0:
-      // Show the first item
-      break;
-    case 1:
-      // Show the second item
-      break;
-    case 2:
-      // Show the third item
-      break;
-  }
-}
-
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -65,21 +88,25 @@ static void window_load(Window *window) {
       .get_num_rows = get_num_rows_callback,
       .draw_row = draw_row_callback,
       .get_cell_height = get_cell_height_callback,
+      .select_click = select_click_handler,
   });
   layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
 }
 
 static void window_unload(Window *window) {
-  menu_layer_destory(s_menu_layer);
+  menu_layer_destroy(s_menu_layer);
+  action_menu_hierarchy_destroy(s_root_level, NULL, NULL);
 }
 
 static void init() {
   s_main_window = window_create();
-  window_set_window_handlers(s_main_window, (WindowHandlers)) {
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
-  }
+  });
   window_stack_push(s_main_window, true);
+
+  init_action_menu();
 }
 
 static void deinit() {
